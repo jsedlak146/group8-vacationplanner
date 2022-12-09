d1f0c59b0f550ea8d5c740540478b5b5b4c6748f
Americano.Dev <andrenations@gmail.com>
2022-12-09 10:50:58 -0500

    this is the new version with the navabar and footer

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,82 @@
+
 var savedLocations = [];
 
 
 var ticketmasterEl = $("#ticketmaster");
 
+const sideNav = document.querySelector(".sidenav");
+  M.Sidenav.init(sideNav, {});
+  
+
+// $(document).ready(function() {
+//     $(".sidenav").sidenav();
+//   });
+
+// $(document).ready(function(){
+//   $(".sidenav").sidenav();
+// });
 
 
-$('#search-location').on('click', function(event){
-    event.preventDefault();
+$('#search-location').on('click', function (event) {
+  event.preventDefault();
 
-    var searchLocation = $('#location-input').val().trim();
-    var searchDateUnformatted = $('#date-input').val().trim();  //value received from user input
-    var ticketmasterStartDate = dayjs(searchDateUnformatted).format('YYYY-MM-DD');  // user input reformatted to ticketmaster required format using dayjs
-    var ticketmasterEndDate = (dayjs(ticketmasterStartDate).add(1, 'day')).format('YYYY-MM-DD'); // user input + 1 day using dayjs
-    
+  
 
-    savedLocations.push(searchLocation);
+  var searchLocation = $('#location-input').val().trim();
+  var searchDateUnformatted = $('#date-input').val().trim();  //value received from user input
+  var ticketmasterStartDate = dayjs(searchDateUnformatted).format('YYYY-MM-DD');  // user input reformatted to ticketmaster required format using dayjs
+  var ticketmasterEndDate = (dayjs(ticketmasterStartDate).add(1, 'day')).format('YYYY-MM-DD'); // user input + 1 day using dayjs
 
-    $("#location-input").val('');
-    
 
-    localStorage.setItem("saved", savedLocations);
-    // console.log(savedLocations);
-    
-    function renderSaved(){
-        $('#location-views').empty();
-    
-        for (var i=0; i< savedLocations.length; i++) {
-            var locBtn = $('<button>');
-            locBtn.addClass('saved-search-button');
-            locBtn.attr('saved', savedLocations[i]);
-            locBtn.text(savedLocations[i]);
-            $('#search-history').append(locBtn);
-        }
-    };
-    
-    renderSaved();
-    clearSearchResults();
-    getTicketMaster(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
+  savedLocations.push(searchLocation);
+
+  $("#location-input").val('');
+
+
+  localStorage.setItem("saved", savedLocations);
+  // console.log(savedLocations);
+
+  function renderSaved() {
+    $('#location-views').empty();
+
+    for (var i = 0; i < savedLocations.length; i++) {
+      var locBtn = $('<button>');
+      locBtn.addClass('saved-search-button');
+      locBtn.attr('saved', savedLocations[i]);
+      locBtn.text(savedLocations[i]);
+      $('#search-history').append(locBtn);
+    }
+  };
+
+  renderSaved();
+  clearSearchResults();
+  getTicketMaster(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
 
 });
+
+
 
 // function to render data received from Ticketmaster API onto cards
 
 function getTicketMaster(location, startDate, endDate) {
   const apiKey = "FtEqYpIoWBFSbSArl0CV46XT95pYEReh";
-  const ticketMasterUrl ="https://app.ticketmaster.com/discovery/v2/events?apikey=" + apiKey + "&locale=*&startDateTime=" + startDate + "T11:10:00Z&endDateTime=" + endDate + "T11:11:00Z&city=" + location;
+  const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + apiKey + "&locale=*&startDateTime=" + startDate + "T11:10:00Z&endDateTime=" + endDate + "T11:11:00Z&city=" + location;
   fetch(ticketMasterUrl)
-    .then(function(response) {
+    .then(function (response) {
 
       return response.json()
 
     })
-    .then(function(data) {
-        console.log((data));
-        for (var i = 0; i < data._embedded.events.length; i++) {
-          var eventName = data._embedded.events[i].name;
-          var eventImage = data._embedded.events[i].images[0].url;
-          var ticketsUrl = data._embedded.events[i].url;
-          var eventDate = dayjs(data._embedded.events[i].dates.start.localDate).format('MMM DD YYYY');
-          var eventTime = data._embedded.events[i].dates.start.localTime;
-      
-          ticketmasterEl.append(`
+    .then(function (data) {
+      console.log((data));
+      for (var i = 0; i < data._embedded.events.length; i++) {
+        var eventName = data._embedded.events[i].name;
+        var eventImage = data._embedded.events[i].images[0].url;
+        var ticketsUrl = data._embedded.events[i].url;
+        var eventDate = dayjs(data._embedded.events[i].dates.start.localDate).format('MMM DD YYYY');
+        var eventTime = data._embedded.events[i].dates.start.localTime;
+
+        ticketmasterEl.append(`
             
               <div class="col s6 m3 l2">
                 <div class="card hoverable ticketmaster-card">
@@ -79,7 +95,7 @@ function getTicketMaster(location, startDate, endDate) {
               </div>
           
           `);
-        };
+      };
     })
 }
 
@@ -87,8 +103,8 @@ function clearSearchResults() {
   ticketmasterEl.empty();
 }
 
- 
-        
-   
 
- 
+
+
+
+
