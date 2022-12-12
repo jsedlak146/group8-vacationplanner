ef86871a1504ce2eb616351c0c061714849ba960
BillStephens2022 <two4onebill@yahoo.com>
2022-12-12 13:02:06 -0500

    updated javascript notes and some other minor changes

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,51 @@
+/* variables for Local Storage - the savedLocations and savedDates will be used to enable user to easily go back
+to previously saved locations/dates */
 var savedLocations = getLocalStorage();
 var savedDates = getLocalStorageDate();
 
-
-
+/* variables to identify elements so that they can be updated dynamically as search results are pulled from the 
+3 API's used (TicketMaster, Open Brewery, and AirBnB) */
 var ticketmasterEl = $("#ticketmaster");
 var breweryEl = $("#breweries");
 var airbnbEl = $("#airbnb");
 
+// function call to render buttons for previously saved locations/dates.
 renderSaved();
 
+// function to pull saved locations from Local Storage.
 function getLocalStorage() {
   return JSON.parse(localStorage.getItem("savedLocations")) || [];
 };
 
+// function to pull saved dates from Local Storage.
 function getLocalStorageDate() {
   return JSON.parse(localStorage.getItem('savedDates')) || [];
 };
 
+// 'click' event listener added to the search button 
 $('#search-location').on('click', function(event){
     event.preventDefault();
-
+    // grabs user entered values for location and dates and formats and saves to variables
     var searchLocation = $('#location-input').val().trim();
     var searchDateUnformatted = $('#date-input').val().trim();  //value received from user input
     var ticketmasterStartDate = dayjs(searchDateUnformatted).format('YYYY-MM-DD');  // user input reformatted to ticketmaster required format using dayjs
     var ticketmasterEndDate = (dayjs(ticketmasterStartDate).add(1, 'day')).format('YYYY-MM-DD'); // user input + 1 day using dayjs
-    
+    // adds saved locations and dates to arrays so that they can be saved to local storage
     savedLocations.push(searchLocation);
     savedDates.push(ticketmasterStartDate);
     $("#location-input").val('');
     $("#date-input").val('');
-    
     localStorage.setItem("savedLocations", JSON.stringify(savedLocations));
     localStorage.setItem("savedDates", JSON.stringify(savedDates));
-    
-    
-    
-    
+    //renders buttons for previously saved locations/dates, clears previous search results from screen and renders new search results from the 3 API's.
     renderSaved();
     clearSearchResults();
     getTicketMaster(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
     getBreweries(searchLocation);
-    getAirbnb(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
-
+    //getAirbnb(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
 });
 
+//function for rendering buttons for previously searched locations/dates.
 function renderSaved(){
   $('#search-history').empty();
 
@@ -57,9 +59,9 @@ function renderSaved(){
       locBtn.text(savedLocations[i]);
       $('#search-history').append(locBtn);
   }
-
 };
 
+// function that will pull search results when search history buttons are clicked.
 function displaySearchHistory () {
 
   var savedCityName = $(this).attr('saved');
@@ -74,7 +76,7 @@ function displaySearchHistory () {
   clearSearchResults();
   getTicketMaster(savedCityName, savedDate, defaultEndDate);
   getBreweries(savedCityName);
-  getAirbnb(savedCityName, savedDate, defaultEndDate);
+  //getAirbnb(savedCityName, savedDate, defaultEndDate);
 
 };
 
@@ -141,12 +143,14 @@ function getTicketMaster(location, startDate, endDate) {
     })
 }
 
+// function for clearing previous results from the 3 API's.
 function clearSearchResults() {
   airbnbEl.empty();
   ticketmasterEl.empty();
   breweryEl.empty();
 }
 
+// function for pulling from Open Breweries API.  Will show a list of local breweries for the city entered by the user.
  function getBreweries(location) {
     
     const breweryUrl = "https://api.openbrewerydb.org/breweries?by_city=" + location + "&per_page=10";
@@ -182,6 +186,7 @@ function clearSearchResults() {
 
  }
 
+ // function for pulling Airbnb listings from the Airbnb API for the city entered by the user.
  function getAirbnb(location, checkInDate, checkOutDate) {
   var settings = {
     method: 'GET',
@@ -238,5 +243,5 @@ function clearSearchResults() {
     .catch(err => console.error(err));
     } 
 
-  
+  // 'click' event listener added to buttons which will call the function for rendering data from previous searches
    $(document).on('click', '.saved-search-button', displaySearchHistory);
