7299379dbd125490dfbffe4a9007428f41af5d0b
Americano.Dev <andrenations@gmail.com>
2022-12-12 15:36:35 -0500

    saving

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,11 @@ var breweryEl = $("#breweries");
 var airbnbEl = $("#airbnb");
 
 var sideNav = document.querySelector(".sidenav");
-  M.Sidenav.init(sideNav, {});
-  
+M.Sidenav.init(sideNav, {});
+
+var ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
 renderSaved();
 
 function getLocalStorage() {
@@ -18,48 +21,48 @@ function getLocalStorageDate() {
   return JSON.parse(localStorage.getItem('savedDates')) || [];
 };
 
-$('#search-location').on('click', function(event){
-    event.preventDefault();
+$('#search-location').on('click', function (event) {
+  event.preventDefault();
 
-    var searchLocation = $('#location-input').val().trim();
-    var searchDateUnformatted = $('#date-input').val().trim();  //value received from user input
-    var ticketmasterStartDate = dayjs(searchDateUnformatted).format('YYYY-MM-DD');  // user input reformatted to ticketmaster required format using dayjs
-    var ticketmasterEndDate = (dayjs(ticketmasterStartDate).add(1, 'day')).format('YYYY-MM-DD'); // user input + 1 day using dayjs
-    
-    savedLocations.push(searchLocation);
-    savedDates.push(ticketmasterStartDate);
-    $("#location-input").val('');
-    $("#date-input").val('');
-    
-    localStorage.setItem("savedLocations", JSON.stringify(savedLocations));
-    localStorage.setItem("savedDates", JSON.stringify(savedDates));
-    
-    
-    renderSaved();
-    clearSearchResults();
-    getTicketMaster(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
-    getBreweries(searchLocation);
-    getAirbnb(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
+  var searchLocation = $('#location-input').val().trim();
+  var searchDateUnformatted = $('#date-input').val().trim();  //value received from user input
+  var ticketmasterStartDate = dayjs(searchDateUnformatted).format('YYYY-MM-DD');  // user input reformatted to ticketmaster required format using dayjs
+  var ticketmasterEndDate = (dayjs(ticketmasterStartDate).add(1, 'day')).format('YYYY-MM-DD'); // user input + 1 day using dayjs
+
+  savedLocations.push(searchLocation);
+  savedDates.push(ticketmasterStartDate);
+  $("#location-input").val('');
+  $("#date-input").val('');
+
+  localStorage.setItem("savedLocations", JSON.stringify(savedLocations));
+  localStorage.setItem("savedDates", JSON.stringify(savedDates));
+
+
+  renderSaved();
+  clearSearchResults();
+  getTicketMaster(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
+  getBreweries(searchLocation);
+  getAirbnb(searchLocation, ticketmasterStartDate, ticketmasterEndDate);
 
 });
 
-function renderSaved(){
+function renderSaved() {
   $('#search-history').empty();
 
-  for (var i=0; i< savedLocations.length; i++) {
-      var locBtn = $('<button>');
-      locBtn.addClass('saved-search-button');
-      locBtn.attr({
-        'saved': savedLocations[i],
-        'saved-date': savedDates[i],
-        });
-      locBtn.text(savedLocations[i]);
-      $('#search-history').append(locBtn);
+  for (var i = 0; i < savedLocations.length; i++) {
+    var locBtn = $('<button>');
+    locBtn.addClass('saved-search-button');
+    locBtn.attr({
+      'saved': savedLocations[i],
+      'saved-date': savedDates[i],
+    });
+    locBtn.text(savedLocations[i]);
+    $('#search-history').append(locBtn);
   }
 
 };
 
-function displaySearchHistory () {
+function displaySearchHistory() {
 
   var savedCityName = $(this).attr('saved');
   var tempDate = $(this).attr('saved-date');
@@ -81,32 +84,32 @@ function displaySearchHistory () {
 
 function getTicketMaster(location, startDate, endDate) {
   const apiKey = "FtEqYpIoWBFSbSArl0CV46XT95pYEReh";
-  const ticketMasterUrl ="https://app.ticketmaster.com/discovery/v2/events?apikey=" + apiKey + "&locale=*&startDateTime=" + startDate + "T11:10:00Z&endDateTime=" + endDate + "T11:11:00Z&city=" + location;
+  const ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + apiKey + "&locale=*&startDateTime=" + startDate + "T11:10:00Z&endDateTime=" + endDate + "T11:11:00Z&city=" + location;
   fetch(ticketMasterUrl)
-    .then(function(response) {
+    .then(function (response) {
 
       return response.json()
 
     })
-    .then(function(data) {
-        console.log((data));
-        ticketmasterEl.append(`
+    .then(function (data) {
+      console.log((data));
+      ticketmasterEl.append(`
            <h2>🎫Local Events🎟</h2>
         `);
-        for (var i = 0; i < data._embedded.events.length; i++) {
-          var eventName = data._embedded.events[i].name;
-          var eventImage = data._embedded.events[i].images[0].url;
-          var ticketsUrl = data._embedded.events[i].url;
-          var eventDate = dayjs(data._embedded.events[i].dates.start.localDate).format('MMM DD YYYY');
-          var eventTime = data._embedded.events[i].dates.start.localTime;
-          var eventHour = parseInt(eventTime.slice(0 , 2));
-          var eventMinutes = eventTime.slice(-5, -3);
-          if (eventHour > 12) {
-            eventTime = (eventHour - 12) + ":" + eventMinutes + " PM";
-          } else {
-            eventTime = eventHour + ":" + eventMinutes + " AM";
-          }
-          ticketmasterEl.append(`
+      for (var i = 0; i < data._embedded.events.length; i++) {
+        var eventName = data._embedded.events[i].name;
+        var eventImage = data._embedded.events[i].images[0].url;
+        var ticketsUrl = data._embedded.events[i].url;
+        var eventDate = dayjs(data._embedded.events[i].dates.start.localDate).format('MMM DD YYYY');
+        var eventTime = data._embedded.events[i].dates.start.localTime;
+        var eventHour = parseInt(eventTime.slice(0, 2));
+        var eventMinutes = eventTime.slice(-5, -3);
+        if (eventHour > 12) {
+          eventTime = (eventHour - 12) + ":" + eventMinutes + " PM";
+        } else {
+          eventTime = eventHour + ":" + eventMinutes + " AM";
+        }
+        ticketmasterEl.append(`
             
               <div class="col s6 m3 l2 id="ticketmaster-div">
                 <div class="card small hoverable ticketmaster-card">
@@ -125,17 +128,17 @@ function getTicketMaster(location, startDate, endDate) {
               </div>
           
           `);
-        };
-        ticketmasterEl.append(`
+      };
+      ticketmasterEl.append(`
         <span id="ticketmaster-span">show more...</span>
         `);
-        $(function () {
-          $('#ticketmaster-span').click(function () {
-              $('#ticketmaster div:hidden').slice(0, 30).show();
-              if ($('#ticketmaster div').length == $('#ticketmaster div:visible').length) {
-                  $('#ticketmaster-span').hide();
-              }
-          });
+      $(function () {
+        $('#ticketmaster-span').click(function () {
+          $('#ticketmaster div:hidden').slice(0, 30).show();
+          if ($('#ticketmaster div').length == $('#ticketmaster div:visible').length) {
+            $('#ticketmaster-span').hide();
+          }
+        });
       });
     })
 }
@@ -146,42 +149,42 @@ function clearSearchResults() {
   breweryEl.empty();
 }
 
- function getBreweries(location) {
-    
-    const breweryUrl = "https://api.openbrewerydb.org/breweries?by_city=" + location + "&per_page=10";
-    fetch(breweryUrl)
-    .then(function(response) {
+function getBreweries(location) {
+
+  const breweryUrl = "https://api.openbrewerydb.org/breweries?by_city=" + location + "&per_page=10";
+  fetch(breweryUrl)
+    .then(function (response) {
 
       return response.json()
-  
+
     })
-    .then(function(data) {
-        console.log((data));
-        breweryEl.append(`
+    .then(function (data) {
+      console.log((data));
+      breweryEl.append(`
            <h2>🍺Local Breweries🍺</h2>
         `);
-        for (var i = 0; i < data.length; i++) {
-          var breweryName = data[i].name;
-          var breweryLocation = data[i].street + " " + data[i].city + ", " + data[i].state;
-          var breweryUrl = data[i].website_url;
-          var breweryUrlAdj;
-          var breweryPhone = data[i].phone;
-          if (breweryUrl === null) {
-            breweryUrlAdj = "No website available.";
-          } else {
-            var breweryUrlAdj = breweryUrl.slice(0, 4) + "s" + breweryUrl.slice(4);
-          }
-          breweryEl.append(`
+      for (var i = 0; i < data.length; i++) {
+        var breweryName = data[i].name;
+        var breweryLocation = data[i].street + " " + data[i].city + ", " + data[i].state;
+        var breweryUrl = data[i].website_url;
+        var breweryUrlAdj;
+        var breweryPhone = data[i].phone;
+        if (breweryUrl === null) {
+          breweryUrlAdj = "No website available.";
+        } else {
+          var breweryUrlAdj = breweryUrl.slice(0, 4) + "s" + breweryUrl.slice(4);
+        }
+        breweryEl.append(`
               <ul>
                 <li class="brewery-names">${breweryName + ", Address: " + breweryLocation + ", Phone: " + breweryPhone + ", url: " + "<a href=" + breweryUrlAdj + ">" + breweryUrlAdj + "</a>"}</li>
               </ul>
           `);
-  };
+      };
     })
 
- }
+}
 
- function getAirbnb(location, checkInDate, checkOutDate) {
+function getAirbnb(location, checkInDate, checkOutDate) {
   var settings = {
     method: 'GET',
     headers: {
@@ -192,11 +195,11 @@ function clearSearchResults() {
   var airbnbUrl = "https://airbnb13.p.rapidapi.com/search-location?location=" + location + "&checkin=" + checkInDate + "&checkout=" + checkOutDate + "&adults=1&children=0&infants=0&page=1"
 
 
-  fetch(airbnbUrl, settings) 
-    .then(function(response){
+  fetch(airbnbUrl, settings)
+    .then(function (response) {
       return response.json()
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
       airbnbEl.append(`
         <h2> Airbnb Listings </h2>
@@ -235,7 +238,8 @@ function clearSearchResults() {
       }
     })
     .catch(err => console.error(err));
-    } 
+}
 
-  
-   $(document).on('click', '.saved-search-button', displaySearchHistory);
+
+$(document).on('click', '.saved-search-button', displaySearchHistory);
+
